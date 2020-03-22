9183b06122ed95029fd88f39ddeb6a48564653bc
Vikum Wijekoon <vikumwijekoon97@gmail.com>
2020-03-22 14:48:24 +0530

    v1.0.2. validation added. styling finalized

--- src/Comps/Controls.jsx
+++ src/Comps/Controls.jsx
@@ -1,126 +1,196 @@
 import React from "react";
-import { Form, Radio, Button } from "semantic-ui-react";
+import {
+  Form,
+  Radio,
+  Button,
+  Container,
+  Header,
+  Divider,
+} from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
 export default function Controls(
-	// }
-	props,
+  // }
+  props
 ) {
-	const { getRootProps, getInputProps } = useDropzone({
-		accept: "image/*",
-		multiple: false,
-		onDrop: acceptedFiles => {
-			acceptedFiles.map(file =>
-				props.handleChange(URL.createObjectURL(file), "image"),
-			);
-		},
-	});
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    multiple: false,
+    onDrop: acceptedFiles => {
+      acceptedFiles.map(file =>
+        props.handleChange(URL.createObjectURL(file), "image")
+      );
+    },
+  });
 
-	return (
-		<>
-			<Form onSubmit={props.handleSubmit}>
-				<Form.Group>
-					{props.FORM_TEXT_DATA.map((item, index) => {
-						if (item.type === "oppName") {
-							return (
-								<Form.TextArea
-									key={item.key}
-									rows={2}
-									value={props[item.type]}
-									{...item.desc}
-									onChange={e => {
-										props.handleChange(e.target.value, item.type);
-									}}
-								/>
-							);
-						}
+  return (
+    <>
+      <Form
+        onSubmit={props.handleSubmit}
+        className="form-container"
+        // size={"large"}
+      >
+        <Container fluid>
+          <Header>Text Controls</Header>
+          <Form.Field
+          //   fluid
+          //   inline
+          // width="equal"
+          >
+            {props.FORM_TEXT_DATA.map((item, index) => {
+              if (item.type === "oppName") {
+                return (
+                  <Form.TextArea
+                    //   inline
+                    key={item.key}
+                    rows={2}
+                    value={props[item.type]}
+                    {...item.desc}
+                    onChange={e => {
+                      props.handleChange(e.target.value, item.type);
+                    }}
+                  />
+                );
+              }
 
-						return (
-							<Form.Input
-								key={item.key}
-								value={props[item.type]}
-								{...item.desc}
-								onChange={e => {
-									props.handleChange(e.target.value, item.type);
-								}}
-							/>
-						);
-					})}
-				</Form.Group>
-				<Form.Group>
-					{props.FORM_CHECKBOX_DATA.map((item, index) => {
-						return (
-							<Form.Checkbox
-								key={item.key}
-								label={item.label}
-								checked={props[item.type]}
-								onChange={() => {
-									props.handleToggle([item.type]);
-									console.log(props[item.type]);
-								}}
-							></Form.Checkbox>
-						);
-					})}
-					<span style={{ marginLeft: "1em", marginRight: "1em" }}> | </span>
-
-					<Form.Radio
-						label="oGE"
-						name="radioGroup"
-						value="oGE"
-						checked={props.OGETSwitch === "oGE"}
-						onChange={props.handleOGETSwitch}
-						// onChange={() => {
-						// 	props.handleOGETSwitch(!props.OGETSwitch);
-						// }}
-					/>
-					<Radio
-						label="oGT"
-						name="radioGroup"
-						value="oGT"
-						checked={props.OGETSwitch === "oGT"}
-						onChange={props.handleOGETSwitch}
-					/>
-				</Form.Group>
-				<Form.Group>
-					<Form.Checkbox
-						label="Custom No."
-						checked={props.phoneSwitch}
-						onChange={() => {
-							props.handleToggle("phoneSwitch");
-						}}
-					></Form.Checkbox>
-					<Form.Input
-						value={props.phoneNo}
-						placeholder={"Input Phone No."}
-						disabled={!props.phoneSwitch}
-						onChange={e => {
-							props.handleChange(e.target.value, "phoneNo");
-						}}
-					/>
-					<span style={{ marginLeft: "1em", marginRight: "1em" }}> | </span>
-					<div {...getRootProps({ className: "dropzone" })}>
-						<input {...getInputProps()} />
-						<p>Drag 'n' drop/click to select images</p>
-					</div>
-				</Form.Group>
-				<div style={{ display: "flex", justifyContent: "center" }}>
-					<Button
-						// className="m1"
-						type="primary"
-						size="large"
-						onClick={props.handleGenerate}
-					>
-						GENERATE
-					</Button>
-					<Button
-						className="m1"
-						type="primary"
-						size="large"
-						onClick={props.handleClear}
-					>
-						RESET
-					</Button>
-				</div>
-			</Form>
-		</>
-	);
+              return (
+                <Form.Input
+                  disabled={
+                    item.type === "salary"
+                      ? props.salarySwitch === false
+                        ? true
+                        : false
+                      : false
+                  }
+                  // inline
+                  key={item.key}
+                  value={props[item.type]}
+                  {...item.desc}
+                  onChange={e => {
+                    props.handleChange(e.target.value, item.type);
+                  }}
+                />
+              );
+            })}
+          </Form.Field>
+        </Container>
+        <Divider
+          style={{
+            width: "90%",
+            margin: "1rem auto 1.5rem auto",
+          }}></Divider>
+        <Container>
+          <Header> Detail Switches</Header>
+          <Form.Group className="checkbox-container">
+            {props.FORM_CHECKBOX_DATA.map((item, index) => {
+              return (
+                <Form.Checkbox
+                  className="checkboxes"
+                  style={{}}
+                  key={item.key}
+                  label={item.label}
+                  checked={props[item.type]}
+                  onChange={() => {
+                    props.handleToggle([item.type]);
+                    console.log(props[item.type]);
+                  }}></Form.Checkbox>
+              );
+            })}
+          </Form.Group>
+        </Container>
+        <Divider
+          style={{
+            width: "90%",
+            margin: "1rem auto 1.5rem auto",
+          }}></Divider>
+        <Container>
+          <Header>Phone No. Details</Header>
+          <div style={{ display: "flex" }}>
+            <Form.Group inline>
+              <Form.Radio
+                className="checkboxes"
+                label="oGE"
+                name="radioGroup"
+                value="oGE"
+                checked={props.OGETSwitch === "oGE"}
+                onChange={props.handleOGETSwitch}
+                // onChange={() => {
+                // 	props.handleOGETSwitch(!props.OGETSwitch);
+                // }}
+              />
+              <Radio
+                className="checkboxes"
+                label="oGT"
+                name="radioGroup"
+                value="oGT"
+                checked={props.OGETSwitch === "oGT"}
+                onChange={props.handleOGETSwitch}
+              />
+            </Form.Group>
+            <Form.Group style={{ marginLeft: "1rem" }}>
+              <Form.Checkbox
+                className="checkboxes phoneno-checkbox"
+                label="Custom No."
+                checked={props.phoneSwitch}
+                onChange={() => {
+                  props.handleToggle("phoneSwitch");
+                }}></Form.Checkbox>
+              <Form.Input
+                style={{ width: "fit-content" }}
+                // label={"Use Custom No."}
+                // inline
+                value={props.phoneNo}
+                placeholder={"Input Phone No."}
+                disabled={!props.phoneSwitch}
+                onChange={e => {
+                  props.handleChange(e.target.value, "phoneNo");
+                }}
+              />
+            </Form.Group>
+          </div>
+        </Container>
+        <Divider
+          style={{
+            width: "90%",
+            margin: "1rem auto 1.5rem auto",
+          }}></Divider>
+        <Container>
+          <Header>Upload Images</Header>
+          <div {...getRootProps({ className: "dropzone" })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop/click to select images</p>
+          </div>
+        </Container>
+        <Divider
+          style={{
+            width: "90%",
+            margin: "1rem auto 1.5rem auto",
+          }}></Divider>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            margin: "1rem",
+          }}>
+          <Button
+            color="blue"
+            disabled={props.disableGen}
+            // style={{ backgroundColor: "#037ef3" }}
+            // className="m1"
+            primary
+            size="large"
+            onClick={props.handleGenerate}>
+            GENERATE
+          </Button>
+          <Button
+            className="m1"
+            type="primary"
+            size="large"
+            onClick={props.handleClear}>
+            RESET
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
 }
--- src/Comps/GeneratedImage.jsx
+++ src/Comps/GeneratedImage.jsx
@@ -15,281 +15,328 @@ const supun = "076 909 2185";
 const anupama = "077 867 5798";
 const YELLOW = "#ffc000";
 const FORM_TEXT_DATA = [
-	{
-		desc: { label: "Country", placeholder: "Enter the Country" },
-		type: "country",
-		key: 0,
-	},
-	{
-		desc: { label: "Salary", placeholder: "Enter the Salary" },
-		type: "salary",
-		key: 1,
-	},
-	{
-		desc: { label: "Opp. Name", placeholder: "Enter the Opp. Name" },
-		type: "oppName",
-		key: 2,
-	},
+  {
+    desc: { label: "Country", placeholder: "Enter the Country" },
+    type: "country",
+    key: 0,
+  },
+  {
+    desc: { label: "Salary", placeholder: "Enter the Salary" },
+    type: "salary",
+    key: 1,
+  },
+  {
+    desc: { label: "Opp. Name", placeholder: "Enter the Opp. Name" },
+    type: "oppName",
+    key: 2,
+  },
 ];
 const FORM_CHECKBOX_DATA = [
-	{ key: 0, label: "Accomodation", type: "accom" },
-	{ key: 1, label: "Food", type: "food" },
-	{ key: 2, label: "Airport Pickup", type: "pickup" },
-	{ key: 3, label: "Salary Switch", type: "salarySwitch" },
+  { key: 0, label: "Accomodation", type: "accom" },
+  { key: 1, label: "Food", type: "food" },
+  { key: 2, label: "Airport Pickup", type: "pickup" },
+  { key: 3, label: "Salary", type: "salarySwitch" },
 ];
 const defaultState = {
-	bgColor: "#092d31b3",
-	country: "Country",
-	sizeControl: 0.25,
-	salary: 500000,
-	food: true,
-	accom: true,
-	pickup: false,
-	salarySwitch: true,
-	count: 0,
-	adjustedHeight: 0,
-	// status: "Provided",
-	oppName: "Opportunity\nName",
-	image: noImg,
-	phoneNo: "Phone No.",
-	OGETSwitch: "o", //true for anupama
-	phoneSwitch: false,
-	midHeight: 300,
-	midWidth: "auto",
-	outHeight: 300,
-	outWidth: "auto",
+  bgColor: "#092d31b3",
+  country: "",
+  sizeControl:
+    window.innerWidth <= 600 ? (window.innerWidth / 2000) * 0.9 : 0.25,
+  salary: "",
+  food: true,
+  accom: true,
+  pickup: false,
+  salarySwitch: true,
+  // count: 0,
+  adjustedHeight: 0,
+  // status: "Provided",
+  oppName: "",
+  image: noImg,
+  phoneNo: "",
+  OGETSwitch: "o", //true for anupama
+  phoneSwitch: false,
+  midHeight: 300,
+  midWidth: "auto",
+  outHeight: 300,
+  outWidth: "auto",
+  disableGen: true,
 };
 export default class GeneratedImage extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.imageNodeRef = React.createRef();
-		this.detailsNodeRef = React.createRef();
-		this.state = {
-			...defaultState,
-		};
-	}
+    this.imageNodeRef = React.createRef();
+    this.detailsNodeRef = React.createRef();
+    this.state = {
+      ...defaultState,
+    };
+  }
 
-	componentDidMount() {
-		this.handleDetailsLineHeight(
-			this.detailsNodeRef,
-			this.state.sizeControl,
-			this.state.adjustedHeight,
-		);
-	}
-	componentDidUpdate(prevProps, prevState) {
-		this.handleDetailsLineHeight(
-			this.detailsNodeRef,
-			this.state.sizeControl,
-			this.state.adjustedHeight,
-		);
-	}
+  componentDidMount() {
+    this.handleDetailsLineHeight(
+      this.detailsNodeRef,
+      this.state.sizeControl,
+      this.state.adjustedHeight
+    );
+  }
+  componentDidUpdate(prevProps, prevState) {
+    this.handleDetailsLineHeight(
+      this.detailsNodeRef,
+      this.state.sizeControl,
+      this.state.adjustedHeight
+    );
+    this.handleValidation(
+      this.state.country,
+      this.state.image,
+      this.state.oppName,
+      this.state.phoneNo,
+      this.state.accom,
+      this.state.food,
+      this.state.pickup,
+      this.state.salarySwitch,
+      this.state.salary,
+      this.state.disableGen
+    );
+  }
+  handleValidation = (
+    country,
+    image,
+    oppName,
+    phoneNo,
+    accom,
+    food,
+    pickup,
+    salarySwitch,
+    salary,
+    disableGen
+  ) => {
+    if (
+      this.state.country &&
+      this.state.image !== noImg &&
+      this.state.oppName &&
+      this.state.phoneNo.length >= 9
+    ) {
+      if (this.state.accom || this.state.food || this.state.pickup) {
+        if (this.state.salarySwitch) {
+          if (this.state.salary) {
+            if (disableGen) {
+              this.setState({ disableGen: false });
+            }
+          } else {
+            if (!disableGen) {
+              this.setState({ disableGen: true });
+            }
+          }
+        } else {
+          if (disableGen) {
+            this.setState({ disableGen: false });
+          }
+        }
+      } else {
+        if (!disableGen) {
+          this.setState({ disableGen: true });
+        }
+      }
+      // console.log("testing");
+    } else {
+      if (!disableGen) {
+        this.setState({ disableGen: true });
+      }
+    }
+  };
+  numberWithCommas = x => {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  };
+  handleChange = (value, type) => {
+    // console.log(value, type);
+    this.setState({ [type]: value });
 
-	numberWithCommas = x => {
-		var parts = x.toString().split(".");
-		parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		return parts.join(".");
-	};
-	handleChange = (value, type) => {
-		console.log(value, type);
+    // console.log("testing");
+  };
+  handleDetailsLineHeight = (
+    detailsNodeRef,
+    sizeControl,
+    stateAdjustedHeight
+  ) => {
+    const height = detailsNodeRef.current.clientHeight;
+    const adjustedHeight = Math.round(height / sizeControl);
+    // console.log(Math.round(adjustedHeight));
+    if (stateAdjustedHeight !== adjustedHeight) {
+      this.setState({ adjustedHeight });
+    }
+  };
 
-		this.setState({ [type]: value });
-		// console.log("testing");
-	};
-	handleDetailsLineHeight = (
-		detailsNodeRef,
-		sizeControl,
-		stateAdjustedHeight,
-	) => {
-		const height = detailsNodeRef.current.clientHeight;
-		const adjustedHeight = Math.round(height / sizeControl);
-		// console.log(Math.round(adjustedHeight));
-		if (stateAdjustedHeight !== adjustedHeight) {
-			this.setState({ adjustedHeight });
-		}
-	};
+  // handleOGETSwitch = (value, type) => {
+  // 	this.setState({ [type]: value });
+  // };
+  handleOGETSwitch = (e, { value }) => {
+    console.log(value);
+    const phoneNo = value === "oGE" ? supun : anupama;
+    this.setState({ OGETSwitch: value, phoneNo });
+  };
+  handleToggle = type => {
+    this.setState(prevState => ({ [type]: !prevState[type] }));
+  };
+  handleClear = () => {
+    this.setState({ ...defaultState });
+  };
+  handleGenerate = () => {
+    this.setState({ sizeControl: 1 }, () => {
+      // setTimeout(() => {
+      let imageNode = this.imageNodeRef;
+      console.log(imageNode.current.offsetTop);
+      dti.toJpeg(imageNode.current, { quality: 0.8 }).then(blob => {
+        window.saveAs(
+          blob,
+          `${this.state.OGETSwitch}-${this.state.country}-${this.state.oppName}.jpg`
+        );
+        // setTimeout(() => {
+        this.setState({ sizeControl: 0.25 });
+      });
+      // }, 200);
+      // }, 400);
+    });
+    // eslint-disable-next-line no-lone-blocks
+    {
+      // setTimeout(() => {
+      // 	let imageNode = this.imageNodeRef;
+      // 	console.log(imageNode.current.offsetTop);
+      // 	// this.imageNodeRef.offsetLeft = 0;
+      // 	// this.imageNodeRef.offsetTop = 0;
+      // 	// imageNode.current.offsetTop = 0;
+      // 	// imageNode.current.offsetLeft = 0;
+      // 	// console.log(this.imageNodeRef);
+      // 	setTimeout(() => {
+      // 		dti.toJpeg(imageNode.current, { quality: 0.8 }).then(blob => {
+      // 			window.saveAs(blob, "image-node.png");
+      // 			// this.setState({ sizeControl: 0.25 });
+      // 		});
+      // 	}, 400);
+      // }, 400);
+    }
+  };
+  render() {
+    // const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
 
-	// handleOGETSwitch = (value, type) => {
-	// 	this.setState({ [type]: value });
-	// };
-	handleOGETSwitch = (e, { value }) => {
-		console.log(value);
-		const phoneNo = value === "oGE" ? supun : anupama;
-		this.setState({ OGETSwitch: value, phoneNo });
-	};
-	handleToggle = type => {
-		this.setState(prevState => ({ [type]: !prevState[type] }));
-	};
-	handleClear = () => {
-		this.setState({ ...defaultState });
-	};
-	handleGenerate = () => {
-		this.setState({ sizeControl: 1 }, () => {
-			// setTimeout(() => {
-			let imageNode = this.imageNodeRef;
-			console.log(imageNode.current.offsetTop);
-			dti.toJpeg(imageNode.current, { quality: 0.8 }).then(blob => {
-				window.saveAs(
-					blob,
-					`${this.state.OGETSwitch}-${this.state.country}-${this.state.oppName}.jpg`,
-				);
-				// setTimeout(() => {
-				this.setState({ sizeControl: 0.25 });
-			});
-			// }, 200);
-			// }, 400);
-		});
-		// eslint-disable-next-line no-lone-blocks
-		{
-			// setTimeout(() => {
-			// 	let imageNode = this.imageNodeRef;
-			// 	console.log(imageNode.current.offsetTop);
-			// 	// this.imageNodeRef.offsetLeft = 0;
-			// 	// this.imageNodeRef.offsetTop = 0;
-			// 	// imageNode.current.offsetTop = 0;
-			// 	// imageNode.current.offsetLeft = 0;
-			// 	// console.log(this.imageNodeRef);
-			// 	setTimeout(() => {
-			// 		dti.toJpeg(imageNode.current, { quality: 0.8 }).then(blob => {
-			// 			window.saveAs(blob, "image-node.png");
-			// 			// this.setState({ sizeControl: 0.25 });
-			// 		});
-			// 	}, 400);
-			// }, 400);
-		}
-	};
-	render() {
-		// const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <Controls
+          FORM_CHECKBOX_DATA={FORM_CHECKBOX_DATA}
+          FORM_TEXT_DATA={FORM_TEXT_DATA}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          handleGenerate={this.handleGenerate}
+          handleClear={this.handleClear}
+          handleToggle={this.handleToggle}
+          country={this.state.country}
+          salary={this.state.salary}
+          food={this.state.food}
+          accom={this.state.accom}
+          pickup={this.state.pickup}
+          disableGen={this.state.disableGen}
+          phoneNo={this.state.phoneNo}
+          OGETSwitch={this.state.OGETSwitch}
+          handlePhoneSwitch={this.handlePhoneSwitch}
+          handleOGETSwitch={this.handleOGETSwitch}
+          phoneSwitch={this.state.phoneSwitch}
+          salarySwitch={this.state.salarySwitch}
+          oppName={this.state.oppName}></Controls>
+        <div
+          style={{
+            // backgroundColor: "#303030",
+            height: `${this.state.sizeControl * 2000}px`,
+            width: `${this.state.sizeControl * 2000}px`,
+          }}>
+          <div
+            className="background"
+            ref={this.imageNodeRef}
+            style={{
+              position: "relative",
+              // backgroundImage: `url(${TempImage})`,
+              height: `${this.state.sizeControl * 2000}px`,
+              width: `${this.state.sizeControl * 2000}px`,
+              backgroundSize: "contain",
+            }}>
+            <div
+              className="Container middleImage"
+              style={{
+                // backgroundImage: `url(${TempImage})`,
+                height: `${this.state.sizeControl * 2000}px`,
+                width: `${this.state.sizeControl * 2000}px`,
 
-		return (
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<Controls
-					FORM_CHECKBOX_DATA={FORM_CHECKBOX_DATA}
-					FORM_TEXT_DATA={FORM_TEXT_DATA}
-					handleSubmit={this.handleSubmit}
-					handleChange={this.handleChange}
-					handleGenerate={this.handleGenerate}
-					handleClear={this.handleClear}
-					handleToggle={this.handleToggle}
-					country={this.state.country}
-					salary={this.state.salary}
-					food={this.state.food}
-					accom={this.state.accom}
-					pickup={this.state.pickup}
-					phoneNo={this.state.phoneNo}
-					OGETSwitch={this.state.OGETSwitch}
-					handlePhoneSwitch={this.handlePhoneSwitch}
-					handleOGETSwitch={this.handleOGETSwitch}
-					phoneSwitch={this.state.phoneSwitch}
-					salarySwitch={this.state.salarySwitch}
-					oppName={this.state.oppName}
-				></Controls>
-				<div
-					style={{
-						// backgroundColor: "#303030",
-						height: `${this.state.sizeControl * 2000}px`,
-						width: `${this.state.sizeControl * 2000}px`,
-					}}
-				>
-					<div
-						className="background"
-						ref={this.imageNodeRef}
-						style={{
-							position: "relative",
-							// backgroundImage: `url(${TempImage})`,
-							height: `${this.state.sizeControl * 2000}px`,
-							width: `${this.state.sizeControl * 2000}px`,
-							backgroundSize: "contain",
-						}}
-					>
-						<div
-							className="Container middleImage"
-							style={{
-								// backgroundImage: `url(${TempImage})`,
-								height: `${this.state.sizeControl * 2000}px`,
-								width: `${this.state.sizeControl * 2000}px`,
-
-								// backgroundSize: "contain",
-							}}
-						>
-							<ImageContainer
-								image={this.state.image}
-								bgColor={this.state.bgColor}
-								// dragHandlers={this.dragHandlers}
-								sizeControl={this.state.sizeControl}
-							></ImageContainer>
-						</div>
-						<div
-							className="Container lines"
-							style={{
-								height: `${this.state.sizeControl * 2000}px`,
-								width: `${this.state.sizeControl * 2000}px`,
-								pointerEvents: "none",
-							}}
-						>
-							<Lines color={YELLOW} height={this.state.adjustedHeight}></Lines>
-						</div>
-						<HeadingText
-							sizeControl={this.state.sizeControl}
-							oppName={this.state.oppName}
-						></HeadingText>
-						<div className="Container texts">
-							<div className="Container-top-text">
-								<CountryText
-									country={this.state.country}
-									sizeControl={this.state.sizeControl}
-								></CountryText>
-							</div>
-							<div className="Container-btm-text">
-								<Details
-									sizeControl={this.state.sizeControl}
-									detailsNodeRef={this.detailsNodeRef}
-									salarySwitch={this.state.salarySwitch}
-									YELLOW={YELLOW}
-									salary={this.state.salary}
-									food={this.state.food}
-									accom={this.state.accom}
-									pickup={this.state.pickup}
-									OGETSwitch={this.state.OGETSwitch}
-									numberWithCommas={this.numberWithCommas}
-								></Details>
-							</div>
-							<div
-								style={{
-									position: "absolute",
-									marginTop: `${this.state.sizeControl * 1742}px`,
-									marginLeft: `${this.state.sizeControl * 1180}px`,
-								}}
-							>
-								<h1
-									className={"whatsapp"}
-									style={{
-										fontSize: `${this.state.sizeControl * 50}px`,
-										fontWeight: "normal",
-									}}
-								>
-									Whatsapp Now!
-								</h1>
-								<h2
-									className="phoneno"
-									style={{
-										fontSize: `${this.state.sizeControl * 80}px`,
-										marginTop: `-${this.state.sizeControl * 12}px `,
-									}}
-								>
-									{this.state.phoneNo}
-								</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+                // backgroundSize: "contain",
+              }}>
+              <ImageContainer
+                image={this.state.image}
+                bgColor={this.state.bgColor}
+                // dragHandlers={this.dragHandlers}
+                sizeControl={this.state.sizeControl}></ImageContainer>
+            </div>
+            <div
+              className="Container lines"
+              style={{
+                height: `${this.state.sizeControl * 2000}px`,
+                width: `${this.state.sizeControl * 2000}px`,
+                pointerEvents: "none",
+              }}>
+              <Lines color={YELLOW} height={this.state.adjustedHeight}></Lines>
+            </div>
+            <HeadingText
+              sizeControl={this.state.sizeControl}
+              oppName={this.state.oppName}></HeadingText>
+            <div className="Container texts">
+              <div className="Container-top-text">
+                <CountryText
+                  country={this.state.country}
+                  sizeControl={this.state.sizeControl}></CountryText>
+              </div>
+              <div className="Container-btm-text">
+                <Details
+                  sizeControl={this.state.sizeControl}
+                  detailsNodeRef={this.detailsNodeRef}
+                  salarySwitch={this.state.salarySwitch}
+                  YELLOW={YELLOW}
+                  salary={this.state.salary}
+                  food={this.state.food}
+                  accom={this.state.accom}
+                  pickup={this.state.pickup}
+                  OGETSwitch={this.state.OGETSwitch}
+                  numberWithCommas={this.numberWithCommas}></Details>
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  marginTop: `${this.state.sizeControl * 1742}px`,
+                  marginLeft: `${this.state.sizeControl * 1180}px`,
+                }}>
+                <h1
+                  className={"whatsapp"}
+                  style={{
+                    fontSize: `${this.state.sizeControl * 50}px`,
+                    fontWeight: "normal",
+                  }}>
+                  Whatsapp Now!
+                </h1>
+                <h2
+                  className="phoneno"
+                  style={{
+                    fontSize: `${this.state.sizeControl * 80}px`,
+                    marginTop: `-${this.state.sizeControl * 12}px `,
+                  }}>
+                  {this.state.phoneNo}
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
